78b266dbd29cf04198b882ee072420952881753c
training <training@mdlslx194>
2016-12-15 18:07:18 +0100

    Thread defini

--- TP4/VIDEO/tp2_video1.cu
+++ TP4/VIDEO/tp2_video1.cu
@@ -11,41 +11,47 @@
 #define BLUE 2
 
 
-	// int k = threadIdx.x + blockIdx.x * blockDim.x ;
-	// int i; //Indice global
-	// int nb_threads = gridDim.x * gridBlock.x;
 
-	// int line_size = width*3,j;
-	// for(i=0; i<k; i++){
-	// 	 for(j=0; j<width; j++){
-	// 		int red   = frame1[i*line_size+3*j+RED];
-	// 		int green = frame1[i*line_size+3*j+GREEN];
-	// 		int blue  = frame1[i*line_size+3*j+BLUE];
-
-	// 		int moy = red/4 + (green*3/4);
-
-	// 		frame1[i*line_size+3*j+RED] = (char)moy;
-	// 		frame1[i*line_size+3*j+GREEN] = (char)moy;
-	// 		frame1[i*line_size+3*j+BLUE] = (char)moy;
-	// 	}
-	// }
 
 
 __global__ void kernel_grey(char * frame1, int width, int length)
-{
-	int i = threadIdx.x; //Indice global
-	int j;
+{	
+
+	// ------ Version 1 seul bloc de height threads ------
+	// int i = threadIdx.x; //Indice global
+	// int j;
+	// int line_size = width*3;
+	// for(j=0; j<width; j++){
+	// 	int red   = frame1[i*line_size+3*j+RED];
+	// 	int green = frame1[i*line_size+3*j+GREEN];
+	// 	int blue  = frame1[i*line_size+3*j+BLUE];
+
+	// 	int moy = red/4 + (green*3/4);
+
+	// 	frame1[i*line_size+3*j+RED] = (char)moy;
+	// 	frame1[i*line_size+3*j+GREEN] = (char)moy;
+	// 	frame1[i*line_size+3*j+BLUE] = (char)moy;
+	// }
+
+	// ------ Version de 16 blocs * 1024 threads ---- 
+	int i = threadIdx.x + blockIdx.x * blockDim.x ;
+	int nb_threads = gridDim.x * blockDim.x;
 	int line_size = width*3;
-	for(j=0; j<width; j++){
-		int red   = frame1[i*line_size+3*j+RED];
-		int green = frame1[i*line_size+3*j+GREEN];
-		int blue  = frame1[i*line_size+3*j+BLUE];
+	int j;
 
-		int moy = red/4 + (green*3/4);
+	
+	for(j=i; j<length; j=j+nb_threads){
+		// if(i*j < length){
+			int red   = frame1[j*3+RED];
+			int green = frame1[j*3+GREEN];
+			// int blue  = frame1[i+j*nb_threads+BLUE];
 
-		frame1[i*line_size+3*j+RED] = (char)moy;
-		frame1[i*line_size+3*j+GREEN] = (char)moy;
-		frame1[i*line_size+3*j+BLUE] = (char)moy;
+			int moy = red/4 + (green*3/4);
+
+			frame1[j*3+RED] = (char)moy;
+			frame1[j*3+GREEN] = (char)moy;
+			frame1[j*3+BLUE] = (char)moy;
+		// }
 	}
 }
 
@@ -53,9 +59,9 @@ __global__ void kernel_grey(char * frame1, int width, int length)
 
 int main (int argc, char * argv[])
 {
-	int i, j, k;
+	// int i, j, k;
 	int cpt_frame;
-	int cpt = 0;
+	// int cpt = 0;
 	int frame_count;
 	int width, height;
 
@@ -84,7 +90,7 @@ int main (int argc, char * argv[])
 	// int line_size = width*3;
 
 	for(cpt_frame = 0; cpt_frame < 100 && cpt_frame < frame_count; cpt_frame ++)
-	{
+	{	
 
 		printf("%d - Read frame with index\n", cpt_frame); fflush(stdout);
 		readFrame_with_index(frame1, cpt_frame);
@@ -92,13 +98,14 @@ int main (int argc, char * argv[])
 
 		if(cpt_frame > 10 && cpt_frame < 100)
 		{
-			cudaMemcpy(frame1_device, frame1, sizeof(char) * width * height * 3, cudaMemcpyHostToDevice);
+			cudaMemcpy(frame1_device, frame1, sizeof(char)*width*height*3, cudaMemcpyHostToDevice);
 			printf("%d - GREY\n", cpt_frame); fflush(stdout);
 
 	        /* COLOR -> GREY */
-	        kernel_grey<<<1 , height>>>(frame1_device, width, height);
+	        // kernel_grey<<<1 , height>>>(frame1_device, width, height);
+	        kernel_grey<<<16 , 64>>>(frame1_device, width, height*width);
 
-	        cudaMemcpy(frame1, frame1_device, sizeof(char) * width * height * 3, cudaMemcpyDeviceToHost);
+	        cudaMemcpy(frame1, frame1_device, sizeof(char)*width*height*3, cudaMemcpyDeviceToHost);
 
 		}
 		writeFrame (frame1);
